2124dc282f337ba8c32fe61da29518483fadc310
Hana1107 <87suzuki1107@gmail.com>
2023-10-21 15:45:32 -0700

    added int array and methods

--- RiscvSimulator/src/RegisterFiles.java
+++ RiscvSimulator/src/RegisterFiles.java
@@ -1,4 +1,13 @@
 
 public class RegisterFiles {
 
+    int[] registers = new int[32];
+
+    public int readFromRegister(int index) {
+        return registers[index];
+    }
+
+    public void writeToRegister(int index, int val) {
+        registers[index] = val;
+    }
 }
