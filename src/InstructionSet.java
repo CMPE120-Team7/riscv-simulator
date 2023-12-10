33e96d2323589ff166f8a83468c5e2c5000910eb
Adelyn Tam <52673806+adelyntam@users.noreply.github.com>
2023-12-10 14:59:09 -0800

    adding examples for instructionset
    addi,add,sub

--- src/InstructionSet.java
+++ src/InstructionSet.java
@@ -1,7 +1,8 @@
 
 public class InstructionSet {
 
-	public static final int ADDI = 0; //change this value to the correct one
-
+	public static final int ADDI = 0x13;
+	public static final int ADD = 0x33;
+    public static final int SUB = 0x40000033;
 
 }
