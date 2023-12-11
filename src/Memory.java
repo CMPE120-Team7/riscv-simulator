bd268581def03cf434ec2a0c9278fc1ce43658f5
nataliekao03 <natkao3011@gmail.com>
2023-12-10 18:36:00 -0800

    add memory class

--- src/Memory.java
+++ src/Memory.java
@@ -1,16 +1,50 @@
 import java.util.HashMap;
 
 public class Memory {
+    private HashMap<Integer, Byte> memory;
 
-    HashMap<Integer, Integer> memory = new HashMap<Integer, Integer>();
+    public Memory() {
+        this.memory = new HashMap<>();
+    }
 
-	public int getSize() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    public void loadInstructions(int[] instructions) {
+        for (int i = 0; i < instructions.length; i++) {
+            writeWord(i * 4, instructions[i]);
+        }
+    }
 
-	public int read(int pc) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    public byte readByte(int address) {
+        validateAddress(address);
+        return memory.getOrDefault(address, (byte) 0);
+    }
+
+    public int readWord(int address) {
+        validateAddress(address);
+        int word = 0;
+
+        for (int i = 0; i < 4; i++) {
+            word |= (readByte(address + i) & 0xFF) << (i * 8);
+        }
+
+        return word;
+    }
+
+    public void writeByte(int address, byte value) {
+        validateAddress(address);
+        memory.put(address, value);
+    }
+
+    public void writeWord(int address, int value) {
+        validateAddress(address);
+
+        for (int i = 0; i < 4; i++) {
+            writeByte(address + i, (byte) (value >> (i * 8)));
+        }
+    }
+
+    private void validateAddress(int address) {
+        if (address < 0) {
+            throw new IllegalArgumentException("Invalid memory address: " + address);
+        }
+    }
 }
